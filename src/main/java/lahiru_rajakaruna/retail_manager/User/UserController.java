f1a52e17d351025e879e97f54e52593637f108e0
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-16 08:19:04 +0530

    add @slf4j

--- src/main/java/lahiru_rajakaruna/retail_manager/User/UserController.java
+++ src/main/java/lahiru_rajakaruna/retail_manager/User/UserController.java
@@ -1,5 +1,7 @@
 package lahiru_rajakaruna.retail_manager.User;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
