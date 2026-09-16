1c2957b4396f85671845f109eae671fd1ed04b33
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-16 07:41:58 +0530

    add ShopRepository

---
+++ src/main/java/lahiru_rajakaruna/retail_manager/Shop/ShopRepository.java
@@ -0,0 +1,11 @@
+package lahiru_rajakaruna.retail_manager.Shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, UUID> {
+}
+
