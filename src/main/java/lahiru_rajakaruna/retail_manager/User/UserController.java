b55e377241cf90a172a805b13be119c063774993
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-16 07:41:20 +0530

    add UserController

---
+++ src/main/java/lahiru_rajakaruna/retail_manager/User/UserController.java
@@ -0,0 +1,35 @@
+package lahiru_rajakaruna.retail_manager.User;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        List<User> users = this.userService.getAllUsers();
+        return users;
+    }
+
+    @GetMapping("/{id}")
+    public User getUserDetails(@PathVariable UUID id) throws Exception {
+        User user = this.userService.getUserById(id);
+        return user;
+    }
+
+
+    @PatchMapping("/{id}")
+    public User updateUser(@RequestBody User userUpdates, @PathVariable UUID id, @RequestParam(name = "field", required = true) String field) throws Exception {
+    }
+
+
+}
