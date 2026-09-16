d499b67f16d1b277b7bcb2f2729b2d3960ebbd94
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-16 08:45:41 +0530

    fix request mapping annotation

--- src/main/java/lahiru_rajakaruna/retail_manager/User/UserController.java
+++ src/main/java/lahiru_rajakaruna/retail_manager/User/UserController.java
@@ -8,7 +8,8 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@RestController("/api/v1/users")
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
     private final UserService userService;
 
