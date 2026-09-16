90aff93eb1eaca53fd67f3bec864a3bc4fb343cc
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-16 07:41:47 +0530

    add ShopDTO

---
+++ src/main/java/lahiru_rajakaruna/retail_manager/Shop/ShopDTO.java
@@ -0,0 +1,22 @@
+package lahiru_rajakaruna.retail_manager.Shop;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@EqualsAndHashCode
+public class ShopDTO {
+    private UUID id;
+    private String name;
+
+    public Optional<String> getName() {
+        return Optional.ofNullable(this.name);
+    }
+}
