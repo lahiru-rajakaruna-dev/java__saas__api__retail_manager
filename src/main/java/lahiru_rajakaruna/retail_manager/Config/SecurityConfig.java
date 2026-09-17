844eb5279b96760652a6b220e41aaf680382dd45
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-17 19:35:17 +0530

    disabled spring security web front

--- src/main/java/lahiru_rajakaruna/retail_manager/Config/SecurityConfig.java
+++ src/main/java/lahiru_rajakaruna/retail_manager/Config/SecurityConfig.java
@@ -2,14 +2,30 @@ package lahiru_rajakaruna.retail_manager.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(16);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSec) {
+        httpSec.csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authRequests -> authRequests.anyRequest().permitAll());
+
+        return httpSec.build();
     }
 }
