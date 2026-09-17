679a1e0b83194fca95adfbf0688ff4597f6b2e8c
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-17 19:35:54 +0530

    remove searchable interface

--- src/main/java/lahiru_rajakaruna/retail_manager/User/IUserRepository.java
+++ src/main/java/lahiru_rajakaruna/retail_manager/User/IUserRepository.java
@@ -1,13 +1,11 @@
 package lahiru_rajakaruna.retail_manager.User;
 
-import lahiru_rajakaruna.retail_manager.AbstractBaseClasses.ISearchable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface IUserRepository
-        extends JpaRepository<User, UUID>, ISearchable<User, UserSearchDTO> {
+public interface IUserRepository extends JpaRepository<User, UUID> {
 
 }
