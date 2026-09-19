b453469d02303f10d46cf742e4ef768c429e094a
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-19 19:57:51 +0530

    refactor used lombok for getters

--- src/main/java/lahiru_rajakaruna/retail_manager/Shop/Shop.java
+++ src/main/java/lahiru_rajakaruna/retail_manager/Shop/Shop.java
@@ -5,21 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lahiru_rajakaruna.retail_manager.AbstractBaseClasses.BaseEntity;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Optional;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
+@Getter
 @Entity
 @Table
 public class Shop extends BaseEntity {
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, updatable = true)
     private String name;
-
-    public Optional<String> getName() {
-        return Optional.ofNullable(this.name);
-    }
 }
