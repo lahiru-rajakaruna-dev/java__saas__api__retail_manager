978253ab7e257b113a6b75c8f34a340e32b2ca9f
Lahiru.Dev <lahiru.rajakaruna.dev@gmail.com>
2026-09-20 08:18:30 +0530

    refactor: add isActive field

--- src/main/java/lahiru_rajakaruna/retail_manager/Shop/ShopDTO.java
+++ src/main/java/lahiru_rajakaruna/retail_manager/Shop/ShopDTO.java
@@ -1,9 +1,6 @@
 package lahiru_rajakaruna.retail_manager.Shop;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -16,9 +13,16 @@ import java.util.UUID;
 public class ShopDTO {
     private UUID id;
     private String name;
+    private boolean isActive;
 
-    public UUID getId() {
-        return this.id;
+    public static ShopDTO convertToDTO(Shop shop) {
+        ShopDTO dto = new ShopDTO();
+
+        dto.setId(shop.getId());
+        dto.setName(shop.getName());
+        dto.setActive(shop.isActive());
+
+        return dto;
     }
 
     public Optional<String> getName() {
